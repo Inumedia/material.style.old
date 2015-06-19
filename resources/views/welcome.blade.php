99800b760670dc863349b7cc90d8a56058e0ea82
Inumedia <inumedia@inumedia.net>
2015-06-19 18:53:03 -0400

    Bootstrap!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,18 +2,37 @@
 <html>
     <head>
         <title>Laravel</title>
-
+        
+        <link href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet">
         <link href='/bootstrap-material-design/dist/css/roboto.min.css' rel='stylesheet' type='text/css'>
-        <link href="/bootstrap-material-design/dist/css/material.min.css" rel="stylesheet" title="Default Style">
+        <link href="/bootstrap-material-design/dist/css/material-fullpalette.min.css" rel="stylesheet" title="Default Style">
+        <link href="/bootstrap-material-design/dist/css/ripples.min.css" rel="stylesheet" title="Default Style">
         <link href="/css/style.css" rel="stylesheet" title="Default Style">
+        <script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+
 
     </head>
     <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Material Styling</div>
-                <div class="quote">Hello world.</div>
-            </div>
+        <div class="header-panel shadow-z-2">
+            <div class="container-fluid"><h1 class="title">Material Styling</h1></div>
         </div>
+        <div class="container-fluid main">
+        <div class="row">
+            <nav class="col-xs-3 menu">
+            <ul>
+                <li>Test</li>
+            </ul>
+            </nav>
+        </div>
+        </div>
+
+        <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.2/js/bootstrap.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/noUiSlider/6.2.0/jquery.nouislider.min.js"></script>
+        <script src="/bootstrap-material-design/dist/js/material.min.js"></script>
+        <script src="/bootstrap-material-design/dist/js/ripples.min.js"></script>
+        <script src="https://cdn.rawgit.com/FezVrasta/dropdown.js/master/jquery.dropdown.js"></script>
+        <script>
+            $("#dropdown-menu select").dropdown();
+        </script>
     </body>
 </html>
