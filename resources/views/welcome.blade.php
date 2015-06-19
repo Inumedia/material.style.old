ef2695149a1463591b1cfe5c27feca6307d82860
Inumedia <inumedia@inumedia.net>
2015-06-19 18:02:49 -0400

    Hello world

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,49 +3,15 @@
     <head>
         <title>Laravel</title>
 
-        <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+        <link href='/bootstrap-material-design/dist/css/roboto.min.css' rel='stylesheet' type='text/css'>
+        <link href="/bootstrap-material-design/dist/css/material.min.css" rel="stylesheet" title="Default Style">
 
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                color: #B0BEC5;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-                margin-bottom: 40px;
-            }
-
-            .quote {
-                font-size: 24px;
-            }
-        </style>
     </head>
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
-                <div class="quote">{{ Inspiring::quote() }}</div>
+                <div class="title">Material Styling</div>
+                <div class="quote">Hello world.</div>
             </div>
         </div>
     </body>
