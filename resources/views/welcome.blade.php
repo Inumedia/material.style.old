4318ce6c103fe2fcb51afe1958b453b960acfdd1
Inumedia <inumedia@inumedia.net>
2015-06-22 05:26:02 -0400

    Overlapping content

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,7 @@
             <h1 class="title">Material Style</h1>
             <h2 class="tagline">The art of designing with the physical world in mind.</h2>
         </div>
-        <div class="container-fluid main">
+        <div class="ccontainer-fluid main col-xs-9 center-block">
             <div class="well page active">
             <p>Hello world! Soon there will be a lot of content here, be ready!
             </div>
